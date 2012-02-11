91f8723d30d5e1c5b901d2bb79ce9d5e3f863e5a
Shane Emmons <semmons99@gmail.com>
2012-02-11 13:38:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uufom::Application.config.secret_token = '285341e012722fefae18d5c5d0d6364f4177cba7fa3efa664e66531681f7135a024d2f2b4dcf0d3c4ab280b3824c7f8885463a681ddbb730db362eb5068c3d15'
