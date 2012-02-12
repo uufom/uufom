55fb8c99f4a57f8edeb103ecca9d84802b3afa28
Shane Emmons <semmons99@gmail.com>
2012-02-11 23:22:46 -0500

    try new caching stuff

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,5 +2,6 @@ class PublicController < ApplicationController
   caches_action :index
 
   def index
+    expires_in 1.day, public: true
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ UUFOM::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=31536000"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
