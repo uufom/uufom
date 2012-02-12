e9cd84a0c88820732c2df6bbd69c83ec674a6306
Shane Emmons <semmons99@gmail.com>
2012-02-11 23:49:06 -0500

    update cache timing

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,7 +3,7 @@ class PublicController < ApplicationController
   caches_action :index
 
   def index
-    expires_in 1.year, public: true
-    fresh_when last_modified: 1.year.ago, public: true
+    expires_in 1.day, public: true
+    fresh_when last_modified: 1.day.ago, public: true
   end
 end
