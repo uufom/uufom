8a27c53d18790274e6d5055b3eb963df409919bc
Shane Emmons <semmons99@gmail.com>
2012-02-11 23:44:20 -0500

    update page caching

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,9 @@
 class PublicController < ApplicationController
+  caches_page   :index
   caches_action :index
 
   def index
-    expires_in 1.day, public: true
+    expires_in 1.year, public: true
+    fresh_when last_modified: 1.year.ago, public: true
   end
 end
