21adbf186c826fd9b9e0a9436cd75c12af999a46
Shane Emmons <shane@emmons.io>
2013-10-30 10:33:32 -0700

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ UUFOM::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store, ENV['MEMCACHIER_SERVERS'].split(","),
-    {:username => ENV['MEMCACHIER_USERNAME'],
-     :password => ENV['MEMCACHIER_PASSWORD']}
+  # config.cache_store = :dalli_store, ENV['MEMCACHIER_SERVERS'].split(","),
+  #   {:username => ENV['MEMCACHIER_USERNAME'],
+  #    :password => ENV['MEMCACHIER_PASSWORD']}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
